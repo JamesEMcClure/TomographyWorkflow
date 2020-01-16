450358b74467248d7bc8425f12bb15bf0c08c686
Suhas Somnath <14300780+ssomnath@users.noreply.github.com>
2020-01-16 18:25:22 -0500

    Using variables instead of hard-coding paths

--- build_tomopy.sh
+++ build_tomopy.sh
@@ -15,10 +15,18 @@ echo "Loaded all necessary modules"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 module list
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Basic folder management
+CWD=$(pwd)
+CONDA_NAME='mcclure-conda'
+PACK_DIR='packages'
+CONDA_DIR=$CWD/$CONDA_NAME
+PACK_DIR=$CWD/$PACK_DIR
+mkdir $PACK_DIR
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Now clone the environment
-conda create --prefix /gpfs/alpine/stf011/world-shared/mcclure/mcclure-conda --clone ibm-wml-ce-1.6.2-0
+conda create --prefix $CONDA_DIR --clone ibm-wml-ce-1.6.2-0
 # Activate clone:
-conda activate /gpfs/alpine/stf011/world-shared/mcclure/mcclure-conda
+conda activate $CONDA_DIR
 # install cython - available via conda for power pc: https://anaconda.org/anaconda/cython
 # Doing this early allows pip installation of other packages
 conda install -y cython
@@ -26,10 +34,11 @@ conda install -y cython
 # install simple packages via pip:
 pip install tifffile scikit-build cftime pyfftw
 # dxchange does not have a pip installer nor power-pc builds on conda so install from source:
-git clone https://github.com/data-exchange/dxchange.git ./packages/dxchange
-cd ./packages/dxchange
+git clone https://github.com/data-exchange/dxchange.git ${PACK_DIR}/dxchange
+cd ${PACK_DIR}/dxchange
 python setup.py install
-cd ../..
+# cd ../..
+cd $CWD
 # Follow instructions from here - https://betterscientificsoftware.github.io/python-for-hpc/summit-mpi4py-note/ to install mpi4py from source
 CC=mpicc 
 MPICC=mpicc 
@@ -38,19 +47,21 @@ pip install mpi4py --no-binary mpi4py
 # Install optional dependencies:
 conda install -y astropy netcdf4
 # dxfile cannot be installed via pip or conda. Install from source:
-git clone https://github.com/data-exchange/dxfile ./packages/DXfile
-cd packages/DXfile/
+git clone https://github.com/data-exchange/dxfile ${PACK_DIR}/DXfile
+cd ${PACK_DIR}/DXfile/
 python setup.py install
-cd ../..
+# cd ../..
+cd $CWD
 # unable to find easy and simple installers for edffile, spefile, olefile
 # Maybe these packages are not required after all
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Tomopy at talks about astra toolbox for GPU reconstruction
 # Install ASTRA Toolbox based on instructions from here - https://www.astra-toolbox.com/docs/install.html#linux-from-source
-git clone https://github.com/astra-toolbox/astra-toolbox.git ./packages/astra-toolbox
-cd ./packages/astra-toolbox/build/linux/
+git clone https://github.com/astra-toolbox/astra-toolbox.git ${PACK_DIR}/astra-toolbox
+cd ${PACK_DIR}/astra-toolbox/build/linux/
 ./autogen.sh   # when building a git version
 ./configure --with-cuda=/sw/summit/cuda/10.1.168 --with-python --with-install-type=module
 make
 make install
-cd ../../../..
+#cd ../../../..
+cd $CWD
